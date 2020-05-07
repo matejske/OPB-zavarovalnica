1f4a1c1d8f0dcb67bb918d8eba2fd31bf5839703
matejske <matej.skerlep@gmail.com>
2020-05-07 14:09:09 +0200

    Priprava datoteke

--- auth_public.py
+++ auth_public.py
@@ -1,4 +1,4 @@
-db = 'banka2020_janos'
+db = 'sem2020_matejs'
 host = 'baza.fmf.uni-lj.si'
 user = 'javnost'
 password = 'javnogeslo'
--- zavarovalnica.py
+++ zavarovalnica.py
@@ -1,8 +1,43 @@
-## meni tole ne dela
-from bottle import route, run
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
 
-@route("/")
-def zacetek():
-    return "To je proba za local host"
+# uvozimo bottle.py
+from bottle import *
 
-run(host="localhost", port=8080)
+# uvozimo ustrezne podatke za povezavo
+import auth_public as auth
+
+# uvozimo psycopg2
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+
+import os
+
+# privzete nastavitve
+SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
+RELOADER = os.environ.get('BOTTLE_RELOADER', True)
+ROOT = os.environ.get('BOTTLE_ROOT', '/')
+DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
+
+# odkomentiraj, če želiš sporočila o napakah
+debug(True)
+######################################################################
+
+
+
+
+
+
+
+
+
+######################################################################
+# Glavni program
+
+# priklopimo se na bazo
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
+#conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+# poženemo strežnik na podanih vratih, npr. http://localhost:8080/
+run(host='localhost', port=SERVER_PORT, reloader=RELOADER)
